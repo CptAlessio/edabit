6263229e579e0af0594b86198735d9440e24a992
Alessio M <alessio.marziali@gmail.com>
2023-05-29 19:03:43 +0100

    .

--- edabitChallenges/Program.cs
+++ edabitChallenges/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel.Design;
 using System.Text;
 using System.Text.RegularExpressions;
+using System.Linq;
 
 namespace CodingChallenges
 {
@@ -214,8 +215,267 @@ namespace CodingChallenges
             }
             return reversedString.ToString();
         }
+        
+        /// <summary>
+        /// Write a function that takes an integer i and returns a string with the integer backwards followed by the original integer.
+        /// To illustrate:
+        /// "123"
+        /// We reverse "123" to get "321" and then add "123" to the end, resulting in "321123".
+        /// Examples
+        /// ReverseAndNot(123) ➞ "321123"
+        /// ReverseAndNot(152) ➞ "251152"
+        /// ReverseAndNot(123456789) ➞ "987654321123456789"
+        /// Notes
+        /// i is a non-negative integer.
+        /// </summary>
+        /// <param name="i"></param>
+        /// <returns></returns>
+        public static string ReverseAndNot(int i)
+        {
+            StringBuilder output = new StringBuilder();
+            var charArrfromStr = i.ToString().ToCharArray();
+            
+            Array.Reverse(charArrfromStr);
+            foreach (char singleCharInReversed in charArrfromStr)
+            {
+                output.Append(singleCharInReversed.ToString());
+            }
 
+            output.Append(i.ToString());
+            return output.ToString();
+        }
+        
+        /*
+         * Create a function that takes an array of integers and strings, converts integers to strings, and
+         * returns the array as a string array.
+         * Examples
+         * ParseArray([1, 2, "a", "b"]) ➞ ["1", "2", "a", "b"]
+         * ParseArray(["abc", 123, "def", 456]) ➞ ["abc", "123", "def", "456"]
+         * ParseArray([1, 2, 3, 17, 24, 3, "a", "123b"]) ➞ ["1", "2", "3", "17", "24", "3", "a", "123b"]
+         * ParseArray([]) ➞ []
+         */
+        public static string[] ParseArray (object[] arr)
+        {
+            List<string> output = new List<string>();
+            foreach (var b in arr)
+            {
+                output.Add(b.ToString());
+            }
+            return output.ToArray();
 
+        }
+        
+        /*Create a function that accepts a string of space separated numbers and returns
+         the highest and lowest number (as a string).
+         Examples
+         HighLow("1 2 3 4 5") ➞ "5 1"
+         HighLow("1 2 -3 4 5") ➞ "5 -3"
+         HighLow("1 9 3 4 -5") ➞ "9 -5"
+         HighLow("13") ➞ "13 13"
+         Notes
+         All numbers are valid Int32, no need to validate them.
+         There will always be at least one number in the input string.
+         Output string must be two numbers separated by a single space, and highest number is first.
+         */
+        public static string HighLow(string str)
+        {
+            var StrSplit = str.Split(' ');
+            int[] strSplitToIntArr = new int[StrSplit.Length];
+
+            for (int i = 0; i < strSplitToIntArr.Length; i++)
+            {
+                strSplitToIntArr[i] = int.Parse(StrSplit[i]);
+            }
+
+            int max = strSplitToIntArr.Max();
+            int min = strSplitToIntArr.Min();
+
+            return max + " " + min;
+        }
+        
+        /*
+         * A strong Scottish accent makes every vowel similar to an "e", so you should replace every
+         * vowel with an "e". Additionally, it is being screamed, so it should be in block capitals.
+         * Create a function that takes a string and returns a string.
+         * Examples
+         * ToScottishScreaming("hello world") ➞ "HELLE WERLD"
+         * ToScottishScreaming("Mr. Fox was very naughty") ➞ "MR. FEX WES VERY NEEGHTY"
+         * ToScottishScreaming("Butterflies are beautiful!") ➞ "BETTERFLEES ERE BEEETEFEL!"
+         * Notes
+         * Make sure to include all punctuation that is in the original string.
+         * You don't need any more namespaces than are already given.
+         */
+        public static string ToScottishScreaming(string str) {
+		    // find every vowel and replace it with e
+            // capitalise
+            var initialArray = str.ToCharArray();
+            StringBuilder builder = new StringBuilder();
+            foreach (char b in initialArray)
+            {
+                // a e o u 
+                if (b.ToString().ToLower().Equals("a") 
+                    || b.ToString().ToLower().Equals("i")
+                    || b.ToString().ToLower().Equals("o") 
+                    || b.ToString().ToLower().Equals("u"))
+                {
+                    builder.Append("e");
+                }
+                else
+                {
+                    builder.Append(b.ToString());
+                }
+            }
+
+            return builder.ToString().ToUpper();
+
+        }
+        
+        
+        /*
+         * Create a function that takes an array of non-negative integers and strings and return a new array without
+         * the strings.
+         * Examples
+         * FilterArray([1, 2, "a", "b"]) ➞ [1, 2]
+         * FilterArray([1, "a", "b", 0, 15]) ➞ [1, 0, 15]
+         * FilterArray([1, 2, "aasf", "1", "123", 123]) ➞ [1, 2, 123]
+         * Notes
+         * Zero is a non-negative integer.
+         * The given array only has integers and strings.
+         * Numbers in the array should not repeat.
+         * The original order must be maintained.
+         */
+        public static int[] FilterArray(object[] arr)
+        {
+            // loop / try parse to int add return
+            List<int> output = new List<int>();
+            foreach (var b in arr)
+            {
+                try
+                {
+                    output.Add(int.Parse(b.ToString()));
+                } catch { }
+            }
+
+            List<int> filtered = new List<int>();
+            foreach (var c in output.ToArray().Distinct())
+            {
+                filtered.Add(c);
+            }
+
+            return filtered.ToArray();
+        }
+        
+        
+        /*
+         * Create a function that takes an array of positive and negative numbers.
+         * Return an array where the first element is the count of positive numbers and the second element is the sum of negative numbers.
+         * Examples
+         * CountPosSumNeg([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, -11, -12, -13, -14, -15]) ➞ [10, -65]
+         * // There are a total of 10 positive numbers.
+         * // The sum of all negative numbers equals -65.
+         * CountPosSumNeg([92, 6, 73, -77, 81, -90, 99, 8, -85, 34]) ➞ [7, -252]
+         * CountPosSumNeg([91, -4, 80, -73, -28]) ➞ [2, -105]
+         * CountPosSumNeg([]) ➞ []
+         * Notes
+         * If given an empty array, return an empty array: []
+         * Cast sum to int, don't mind the remaining decimal places.
+         * 0 is not positive.
+         */
+        public static int[] CountPosSumNeg(double[] arr) 
+        {
+            // loop find negative and positives
+            // add positives to array1 add negatives to array2
+            // sum positives sum negatives add to a third array
+
+            if (arr.Length == 0)
+            {
+                int[] b = new int[0];
+                return b;
+            }
+
+            List<double> positives = new List<double>();
+            List<double> negatives = new List<double>();
+
+            foreach (double c in arr)
+            {
+                if (c > 0)
+                {
+                    positives.Add(c);
+                }
+            }
+
+            foreach (double d in arr)
+            {
+                if (d < 0)
+                {
+                    negatives.Add(d);
+                }
+            }
+
+            var negativeSum = negatives.AsQueryable().Sum().ToString();
+            
+            List<int> output = new List<int>();
+            output.Add(positives.Count);
+            output.Add(int.Parse(negativeSum));
+            return output.ToArray();
+
+        }
+        
+        /*
+         * Write a function that returns the greatest common divisor (GCD) of two integers.
+         * Examples
+         * gcd(32, 8) ➞ 8
+         * gcd(8, 12) ➞ 4
+         * gcd(17, 13) ➞ 1
+         * Notes
+         * Both values will be positive.
+         * The GCD is the largest factor that divides both numbers.
+         */
+        public static int gcd(int n1, int n2)
+        {
+            while (n2 != 0)
+            {
+                int temp = n2;
+                n2 = n1 % n2;
+                n1 = temp;
+            }
+            return n1;
+        }
+        
+        /*
+         * Usually when you sign up for an account to buy something, your credit card number, phone number or answer
+         * to a secret question is partially obscured in some way. Since someone could look over your shoulder,
+         * you don't want that shown on your screen. Hence, the website masks these strings.
+         * Your task is to create a function that takes a string, transforms all but the last four characters
+         * into "#" and returns the new masked string.
+         * Examples
+         * Maskify("4556364607935616") ➞ "############5616"
+         * Maskify("64607935616") ➞ "#######5616"
+         * Maskify("1") ➞ "1"
+         * Maskify("") ➞ ""
+         * Notes
+         * The maskify function must accept a string of any length.
+         * An empty string should return an empty string (fourth example above).
+         */
+        public static string Maskify(string str) 
+        {
+            var strLen = str.Length;
+            var strStartFrom = strLen - 4;
+            var toArr = str.ToArray();
+            var builder = new StringBuilder();
+            for (int i = 0; i < strLen - 4; i++)
+            {
+                builder.Append("#");
+            }
+
+            for (int i = strStartFrom; i < strLen; i++)
+            {
+                builder.Append(toArr[i]);
+            }
+
+            return builder.ToString();
+        }
+        
         /// <summary>
         /// Main to test algorithms
         /// </summary>
@@ -229,7 +489,13 @@ namespace CodingChallenges
             //IndexOfCapitals("Fo?.arg~{86tUx=|OqZ!");
             //Console.WriteLine(CountOnes(12));
             //Console.WriteLine(Bomb("Hey, did you find the BoMb?"));
-            Console.WriteLine(ReverseCase("Happy Birthday"));
+            //Console.WriteLine(ReverseCase("Happy Birthday"));
+            //Console.WriteLine(ReverseAndNot(123));
+            //Console.WriteLine(HighLow("4 5 29 54 4 0 -214 542 -64 1 -3 6 -6"));
+            //FilterArray(new object[] { 1, 2, "aasf", "1", "123", 123 });
+            //CountPosSumNeg(new double[] { 92, 6, 73, -77, 81, -90, 99, 8, -85, 34 });
+            //gcd(8, 12);
+            //Console.WriteLine(Maskify("4556364607935616"));
         }
     }
 }
